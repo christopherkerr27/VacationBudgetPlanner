f9ec23227665f50b7775c305c0fe30a5a9e7bf02
Christopher Kerr <christopherke@truck-hero.com>
2021-04-26 07:50:43 -0400

    first commit

---
+++ VacationBudgetPlanner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VacationBudgetPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the vacation budget planner app.");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}, What country would you like to travel to: Singapore or Bali?");
+        }
+    }
+}
